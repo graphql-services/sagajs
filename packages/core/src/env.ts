0f4e44a121c2e44377f484f48ba3ddc282b68413
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-10-24 16:26:08 +0200

    Remove unnecessary code

--- packages/core/src/env.ts
+++ packages/core/src/env.ts
@@ -2,15 +2,6 @@ import { config } from 'dotenv';
 
 config({});
 
-// interface IENV {
-//   PORT: number | string;
-//   DEBUG: boolean;
-//   NODE_ENV: string;
-//   NSQ_URL: string;
-//   API_URL: string;
-//   SENTRY_DNS?: string;
-// }
-
 export const ENV = process.env as { [key: string]: string };
 
 export const getENV = (key: string, name?: string): string => {
